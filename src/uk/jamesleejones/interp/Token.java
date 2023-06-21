8dbca6e211a3604916e110a23545d437a6b1f847
James Lee-Jones <james@lee-jones.co.uk>
2023-06-22 00:09:11 +0100

    Add a scanner
    Add a Scanner class to convert input programs into Tokens that can be
    used by the rest of the interpreter.
    
    Fixes #1.

---
+++ src/uk/jamesleejones/interp/Token.java
@@ -0,0 +1,21 @@
+package uk.jamesleejones.interp;
+
+public class Token {
+  final TokenType type;
+  final String lexeme;
+  final Object literal;
+  final int line;
+
+  Token(TokenType type, String lexeme, Object literal, int line) {
+    // TODO: Possibly support column information
+    this.type = type;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.line = line;
+  }
+
+  @Override
+  public String toString() {
+    return type + " " + lexeme + " " + literal;
+  }
+}
